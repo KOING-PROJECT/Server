d0be975536a5550b1838315ef546581866c8e3e9
SonMinGyu <littleking13@naver.com>
2022-10-10 00:02:33 +0900

    initial commit

---
+++ src/main/java/com/koing/server/koing_server/KoingServerApplication.java
@@ -0,0 +1,13 @@
+package com.koing.server.koing_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koing/server/koing_server/KoingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koing.server.koing_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
