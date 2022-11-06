c68f638294dafb6938466a52b45f44d4b696e617
SonMinGyu <littleking13@naver.com>
2022-11-06 18:50:33 +0900

    [REFACTOR] User domain 리팩토링
    - User domain을 리팩토링 함
    - 몇몇 속성을 추가, 삭제하고 UserOptionalInfo domain을 OneToOne 관계로 Mapping

--- src/main/java/com/koing/server/koing_server/domain/user/User.java
+++ src/main/java/com/koing/server/koing_server/domain/user/User.java
@@ -11,13 +11,18 @@ import javax.persistence.*;
 public class User extends AuditingTimeEntity {
 
     @Builder
-    public User(String name, GenderType gender, int age, String phoneNumber, String address, String imageUrl) {
+    public User(String email, String password
+            , String phoneNumber, String name, String birthDate
+            , String country, GenderType gender, int age, UserOptionalInfo userOptionalInfo) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
         this.name = name;
+        this.birthDate = birthDate;
+        this.country = country;
         this.gender = gender;
         this.age = age;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.imageUrl = imageUrl;
+        this.userOptionalInfo = null;
     }
 
     @Id
@@ -25,7 +30,22 @@ public class User extends AuditingTimeEntity {
     private Long id;
 
     @Column(length = 30, nullable = false)
+    private String email;
+
+    @Column(length = 16, nullable = false)
+    private String password;
+
+    @Column(length = 30)
+    private String phoneNumber;
+
+    @Column(length = 30, nullable = false)
     private String name;
+
+    @Column(length = 20, nullable = false)
+    private String birthDate;
+
+    @Column(length = 50, nullable = false)
+    private String country;
 
     @Enumerated(EnumType.STRING)
     private GenderType gender;
@@ -33,32 +53,27 @@ public class User extends AuditingTimeEntity {
     @Column
     private int age;
 
-    @Column(length = 30)
-    private String phoneNumber;
-
-    @Column(length = 100)
-    private String address;
-
-    @Column(length = 300)
-    private String imageUrl;
-
-
-    public static User newInstance(String name, GenderType gender, int age, String phoneNumber, String address, String imageUrl) {
-        return User.builder()
-                .name(name)
-                .gender(gender)
-                .age(age)
-                .phoneNumber(phoneNumber)
-                .address(address)
-                .imageUrl(imageUrl)
-                .build();
-    }
-
+    // orphanRemoval = true 이므로 부모 entity에서 자식 entity를 삭제하면 자식 entity가 삭제됨.
+    @JoinColumn(name = "userOptionalInfoId")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserOptionalInfo userOptionalInfo;
 
 //    소셜 로그인시 사용
 //    private SocialInfo socialInfo;
 
 
-
-
+    public static User newInstance(String email, String password, String phoneNumber,
+            String name, String birthDate, String country, GenderType gender, int age) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .name(name)
+                .birthDate(birthDate)
+                .country(country)
+                .gender(gender)
+                .age(age)
+                .userOptionalInfo(null)
+                .build();
+    }
 }
