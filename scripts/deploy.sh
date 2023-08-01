c363137439488f576949559b7e9ff6ac8ae657c1
SonMinGyu <littleking13@naver.com>
2023-08-01 15:53:21 +0900

    [CHORE] deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,4 +26,4 @@ echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar $JAR_NAME /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=dev $JAR_NAME /dev/null 2> /dev/null < /dev/null &
