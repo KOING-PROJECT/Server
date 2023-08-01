40caa9beeb12a6d57a31aad4db7643dbc5d02d40
SonMinGyu <littleking13@naver.com>
2023-08-01 16:24:27 +0900

    [CHORE] deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/koing_server
-PROJECT_NAME=KOING_server-0.0.1-SNAPSHOT-boot
+PROJECT_NAME=KOING_server-0.0.1-SNAPSHOT-boot.jar
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/build/libs/$PROJECT_NAME.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/$PROJECT_NAME $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
