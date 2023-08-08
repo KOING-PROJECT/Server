29f695bb04f78f3650a62c909bcb9050a49ad883
SonMinGyu <littleking13@naver.com>
2023-08-08 16:58:30 +0900

    [CHORE] deploy.sh 파일 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,10 +9,10 @@ if [ -z $IS_GREEN  ];then # blue라면
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  sudo docker-compose pull green # green으로 이미지를 내려받습니다.
+  docker-compose pull green # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  sudo docker-compose up -d green # green 컨테이너 실행
+  docker-compose up -d green # green 컨테이너 실행
 
 #  echo "1. get green image"
 #  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml pull green
@@ -37,17 +37,17 @@ if [ -z $IS_GREEN  ];then # blue라면
   sudo nginx -s rel
 
   echo "5. blue container down"
-  sudo docker-compose stop blue
+  docker-compose stop blue
   sudo docker image prune -af
 
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  sudo docker-compose pull blue
+  docker-compose pull blue
 
   echo "2. blue container up"
-  sudo docker-compose up -d blue
+  docker-compose up -d blue
 
 #  echo "1. get green image"
 #  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml pull blue
@@ -71,6 +71,6 @@ else
   sudo nginx -s reload
 
   echo "5. green container down"
-  sudo docker-compose stop green
+  docker-compose stop green
   sudo docker image prune -af
 fi
