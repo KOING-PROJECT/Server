83c93dbcd7190b7573955ca463609276dce34ccd
SonMinGyu <littleking13@naver.com>
2023-08-08 16:26:24 +0900

    [CHORE] deploy.sh 파일 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
+IS_GREEN=$(docker ps | grep koing-green) # 현재 실행중인 App이 blue인지 확인합니다.
 DEFAULT_CONF=" /etc/nginx/nginx.conf"
+DOCKER_APP_NAME=koing
 
 if [ -z $IS_GREEN  ];then # blue라면
 
   echo "### BLUE => GREEN ###"
 
+#  echo "1. get green image"
+#  docker-compose pull green # green으로 이미지를 내려받습니다.
+#
+#  echo "2. green container up"
+#  docker-compose up -d green # green 컨테이너 실행
+
   echo "1. get green image"
-  docker-compose pull green # green으로 이미지를 내려받습니다.
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml pull green
 
   echo "2. green container up"
-  docker-compose up -d green # green 컨테이너 실행
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d green
 
   while [ 1 = 1 ]; do
   echo "3. green health check..."
@@ -33,11 +40,17 @@ if [ -z $IS_GREEN  ];then # blue라면
 else
   echo "### GREEN => BLUE ###"
 
-  echo "1. get blue image"
-  docker-compose pull blue
+#  echo "1. get blue image"
+#  docker-compose pull blue
+#
+#  echo "2. blue container up"
+#  docker-compose up -d blue
 
-  echo "2. blue container up"
-  docker-compose up -d blue
+  echo "1. get green image"
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.blue.yml pull blue
+
+  echo "2. green container up"
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.blue.yml up -d blue
 
   while [ 1 = 1 ]; do
     echo "3. blue health check..."
