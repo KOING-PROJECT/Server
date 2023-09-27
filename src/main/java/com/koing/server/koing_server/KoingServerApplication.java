56a6a6d5dfc70d68f9566eae5bcb38f6a1977609
SonMinGyu <littleking13@naver.com>
2023-09-27 10:53:38 +0900

    [CHORE] 재배포

--- src/main/java/com/koing/server/koing_server/KoingServerApplication.java
+++ src/main/java/com/koing/server/koing_server/KoingServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class KoingServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KoingServerApplication.class, args);
     }
