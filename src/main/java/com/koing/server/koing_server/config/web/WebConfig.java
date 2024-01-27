8b0998c0a61f1f3c9430db5158c09c2d10463156
SonMinGyu <littleking13@naver.com>
2024-01-27 17:47:58 +0900

    refactor: Spring Security 설정 변경

--- src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
+++ src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://koing-project.github.io")
+                .allowedOrigins("https://koing-project.github.io", "https://koing.store")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
