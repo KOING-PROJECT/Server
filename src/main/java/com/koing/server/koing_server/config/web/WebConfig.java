119197fde53b31cad8b0859af8b1a5bceca3e084
SonMinGyu <littleking13@naver.com>
2024-01-28 16:21:06 +0900

    refactor: URL 접근 권한 롤백

--- src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
+++ src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://koing-project.github.io")
-////                .allowedOrigins("https://koing-project.github.io", "https://koing.store")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .maxAge(3600);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://koing-project.github.io", "https://koing.store")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
 
 }
