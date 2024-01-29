695b47859e24c1508c0e660193c7bc06bcddfddb
SonMinGyu <littleking13@naver.com>
2024-01-29 21:05:24 +0900

    refactor: webConfig 설정 수정

--- src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
+++ src/main/java/com/koing/server/koing_server/config/web/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://koing-project.github.io", "https://koing.store")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .maxAge(3600);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://koing-project.github.io", "https://koing.store", "http://hi.com")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
 
 }
